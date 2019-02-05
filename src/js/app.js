4921a6488391f576685136d0e480fa802eb9cad9
AnaPaulaMOntap <anapaula.monsalvet@gmail.com>
2019-02-04 22:40:45 -0300

    Cambios id

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,11 @@
 
-export const writeUserData =(uid, name, email, imageUrl)=> {
-    firebase.database().ref('users/' + uid).set({
+export const writeUserData = (uid, name, email, imageUrl) => {
+  firebase.database().ref('users/'+uid).set({
+    "perfil": {
       username: name !== null ? name : firebase.auth().currentUser.email,
       email: email,
-      profile_picture : imageUrl !== null ? imageUrl : false,
-    });
-  }
+      profile_picture: imageUrl !== null ? imageUrl : false,
+    }
+      
+  });
+}
