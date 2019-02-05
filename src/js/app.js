e7887a62ebad8d20fc97be006415503807206ca5
LeslieSaVa <leslie.sabattin@usach.cl>
2019-02-05 18:20:19 -0300

    actualizando cambios app y html

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,12 @@
 
 export const writeUserData =(uid, name, email, imageUrl)=> {
-    firebase.database().ref('users/' + uid).set({
+    firebase.database().ref('users/'+ uid).set({
+      'perfil':{
       username: name !== null ? name : firebase.auth().currentUser.email,
       email: email,
       profile_picture : imageUrl !== null ? imageUrl : false,
+      }
     });
   }
+
+
