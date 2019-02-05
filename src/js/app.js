a27ce2e029d00a76644aba984367ff9ec7f581e2
AnaPaulaMOntap <anapaula.monsalvet@gmail.com>
2019-02-05 18:15:35 -0300

    Se crean comentarios en pantalla y en base de Datos firebase

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,5 @@
 
+
 export const writeUserData = (uid, name, email, imageUrl) => {
   firebase.database().ref('users/'+uid).set({
     "perfil": {
@@ -9,3 +10,48 @@ export const writeUserData = (uid, name, email, imageUrl) => {
       
   });
 }
+
+export const enviarConvalidacionAFirebase =(uid, username,title, body)=>{
+  // Crear nuevo post
+  const postData = {
+    author: username,
+    uid: uid,
+    body: body,
+    title: title,
+    
+  };
+
+  // Llave que identifica el nuevo post
+  var newPostKey = firebase.database().ref().child('posts').push().key;
+
+  // Ingresa el post en publico y en su perfil.
+  var updates = {};
+  updates['/posts/' + newPostKey] = postData;
+  updates['/users/' + uid + '/post/' + newPostKey] = postData;
+
+  return firebase.database().ref().update(updates);
+}
+
+export const readPost = (onpostChange) => {
+  var postRef = firebase.database().ref('posts');
+  postRef.on('child_added', (coment)=> {
+    onpostChange(coment);
+  });
+};
+
+
+export function deletePost(){
+ 
+  firebase.database().ref('posts/'+ this.id).set({
+    null:null
+  })
+  firebase.database().ref('/users/' + firebase.auth().currentUser.uid+ '/post/'+ this.id).set({
+    null:null
+  })
+
+};
+
+
+
+
+
