f84ef71c682806f9a673f837446a3524f9cd186f
AnaPaulaMOntap <anapaula.monsalvet@gmail.com>
2019-02-07 13:48:26 -0300

    Se crea perfil del usuario Google/Facebook en Firebase

--- src/js/app.js
+++ src/js/app.js
@@ -9,13 +9,14 @@ export const writeUserData = (uid, name, email, imageUrl) => {
   });
 }
 
-export const enviarConvalidacionAFirebase =(uid, username,title, body)=>{
+export const enviarConvalidacionAFirebase =(uid, username,title, body,imagen)=>{
   // Crear nuevo post
   const postData = {
     author: username,
     uid: uid,
     body: body,
     title: title,
+    imagen: imagen !== null ? imagen : false,
     
   };
 
@@ -32,12 +33,12 @@ export const enviarConvalidacionAFirebase =(uid, username,title, body)=>{
 
 export const readPost = (onpostChange) => {
   var postRef = firebase.database().ref('posts');
-  postRef.on('child_added',(coment)=> {
+  postRef.once('child_added',(coment)=> {
     onpostChange(coment);
   });
 };
 
-
+/*
 export function deletePost(){ 
   firebase.database().ref('posts/'+ this.id).set({
     null:null
@@ -46,7 +47,53 @@ export function deletePost(){
     null:null
   })
 };
+*/
 
+
+
+
+
+
+export const mostrarImgFirebase=(imgChange)=>{
+  imagenRef = firebase.database().ref('posts');
+  imagenRef.on("value", function(snapshot){
+     imgChange(snapshot)
+  });
+}
+
+export const uploadImgtoFirebase=()=>{
+  const imagenASubir = fichero.files[0];
+  const uploadTask = firebase.storage().ref().child('imagenes/' + imagenASubir.name).put(imagenASubir);
+  uploadTask.on('state_changed', 
+  function(snapshot){
+    // Observe state change events such as progress, pause, and resume
+    // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+    console.log('Upload is ' + progress + '% done');
+    switch (snapshot.state) {
+      case firebase.storage.TaskState.PAUSED: // or 'paused'
+        console.log('Upload is paused');
+        break;
+      case firebase.storage.TaskState.RUNNING: // or 'running'
+        console.log('Upload is running');
+        break;
+    }
+  }, function(error) {
+      alert("hubo un error");
+    // Handle unsuccessful uploads
+  }, function() {
+    // Handle successful uploads on complete
+    // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+    uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+      console.log('File available at', downloadURL);
+      crearNodoEnBDFirebase(imagenASubir.name, downloadURL);
+    });
+  });
+}
+
+export const crearNodoEnBDFirebase=(nombreImagen, downloadURL)=>{
+  imagenesRef.push({nombre: nombreImagen, url: downloadURL});
+}
 
 
 
