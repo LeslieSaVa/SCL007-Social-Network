43a1a92df8782e823bf228432ec5098158e11c0a
GloriaMoraga <silmeriel@gail.com>
2019-02-11 16:58:52 -0300

    trying like functions

--- src/js/app.js
+++ src/js/app.js
@@ -44,6 +44,24 @@ export const readPost = (onpostChange) => {
   });
 };
 
+
+
+// export const saveLikePost = (postID) => {
+//   let userID = firebase.auth().currentUser.uid;
+//   let postRef = firebase.database().ref(`timeline`);
+
+//   postRef.once("value", function (snapshot) {
+
+//     let likeExists = snapshot.child(postID).child("likes").child(userID).val();
+
+//     if (likeExists === null) {
+//       postRef.child(postID).child("likes").child(userID).set(1)
+//     } else {
+//       postRef.child(postID).child("likes").child(userID).remove();
+//     }
+//   });
+// }
+
 // export const deletePost = () => {
 //   var userID = deletePost1.target.getAttribute("userid");   //userid="${coment.key}"
 //   var firebaseref = firebase.database().ref('posts/'+ userID).delete();
